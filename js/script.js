6640c7b85a123fe67e7b6977071aad92614cb312
Alfir Zagidullin <alfir1991@gmail.com>
2022-07-31 23:49:54 +0300

    Испоавлены замечания

--- js/script.js
+++ js/script.js
@@ -1,24 +1,26 @@
 'use strick';
 
-const DomElement = function(selector, height, width, bg, fontSize) {
-  this.selector = '.block';
-  this.height = 100;
-  this.width = 100;
-  this.bg = 'green';
-  this.fontSize = 14;
+const DomElement = function(selector, height = 100, width = 100, bg = 'red', fontSize = 20) {
+  this.selector = selector;
+  this.height = height;
+  this.width = width;
+  this.bg = bg;
+  this.fontSize = fontSize;
 
   this.getBlock = function () {
     if (this.selector[0] == '.') {
-      document.body.innerHTML = '<div class="' + this.selector.slice(1) + '">New div</div>';
+      document.body.insertAdjacentHTML('afterbegin', `<div class="${this.selector.slice(1)}">New div</div>`);
     } else if (this.selector[0] == '#') {
-      document.body.innerHTML = '<p id="' + this.selector.slice(1) + '">New paragraph</p>'; 
+      document.body.insertAdjacentHTML('afterbegin', `<p id="${this.selector.slice(1)}">New paragraph</p>`);
     }
-    document.querySelector('div, p').style.cssText = 'height: ' + this.height + 'px; width: ' + this.width + 'px; background: ' + this.bg + '; font-size: ' + this.fontSize + 'px';
+    document.querySelector('div, p').style.cssText = `height: ${this.height}px; width: ${this.width}px; background: ${this.bg}; fontSize: ${this.fontSize}px;`;
   };
 };
 
 const element = new DomElement();
 
-element.selector = '#best';
+
+element.selector = '.block';
 
 element.getBlock();
+
