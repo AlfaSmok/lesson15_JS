f647bb643ffc17ff6da3f68a08d9501a50bf700b
Alfir Zagidullin <alfir1991@gmail.com>
2022-07-31 18:59:05 +0300

    Исправлены замечания

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 'use strick';
 
-const DomElement = function() {
-  this.selector = '';
+const DomElement = function(selector, height, width, bg, fontSize) {
+  this.selector = '.block';
   this.height = 100;
   this.width = 100;
   this.bg = 'green';
@@ -10,16 +10,15 @@ const DomElement = function() {
   this.getBlock = function () {
     if (this.selector[0] == '.') {
       document.body.innerHTML = '<div class="' + this.selector.slice(1) + '">New div</div>';
-      document.querySelector('div').style.cssText = 'height: ' + this.height + 'px; width: ' + this.width + 'px; background: ' + this.bg + '; font-size: ' + this.fontSize + 'px';
     } else if (this.selector[0] == '#') {
-      document.body.innerHTML = '<p id="' + this.selector.slice(1) + '">New paragraph</p>';
-      document.querySelector('p').style.cssText = 'height: ' + this.height + 'px; width: ' + this.width + 'px; background: ' + this.bg + '; font-size: ' + this.fontSize + 'px';
+      document.body.innerHTML = '<p id="' + this.selector.slice(1) + '">New paragraph</p>'; 
     }
+    document.querySelector('div, p').style.cssText = 'height: ' + this.height + 'px; width: ' + this.width + 'px; background: ' + this.bg + '; font-size: ' + this.fontSize + 'px';
   };
 };
 
 const element = new DomElement();
 
-element.selector = '.block';
+element.selector = '#best';
 
 element.getBlock();
